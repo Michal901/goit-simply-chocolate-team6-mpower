e3e481c6480142184d4d8e276b66627fa2f04b35
Michal901 <143707542+Michal901@users.noreply.github.com>
2023-11-21 12:03:36 +0000

    Deploying to gh-pages from @ Michal901/goit-simply-chocolate-team6-mpower@1285e57727f2ccf353d0548b95855684ceb31943 🚀

--- index.253d8b49.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openModalBtn:document.querySelector("[data-modal-subscribe-open]"),closeModalBtn:document.querySelector("[data-modal-subscribe-close]"),modal:document.querySelector("[data-modal-subscribe]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
-//# sourceMappingURL=index.253d8b49.js.map
--- index.ca0130ae.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openModalBtn:document.querySelector("[data-modal-reviews-open]"),closeModalBtn:document.querySelector("[data-modal-reviews-close]"),modal:document.querySelector("[data-modal-reviews]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
-//# sourceMappingURL=index.ca0130ae.js.map
